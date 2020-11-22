2d67f475aad1abb124d4efb88988fa47b76b57bd
Jorge Torres <dr.torres@playahealth.org>
2020-11-22 13:40:39 +0100

    Created new Files and learned first operators

---
+++ arithmetic.js
@@ -0,0 +1,20 @@
+'use strict';
+//SUM
+console.log(1+1);
+// Mod operator
+console.log(7%2);
+
+let value = 2;
+//increment Operator
+value ++;
+console.log(value) ;
+//Prefix increment Operator
+++ value;
+console.log(value) ;
+
+//Decrement Operator
+--value;
+console.log(value) ;
+//
+value--;
+console.log(value) ;
---
+++ declaringVariables.js
@@ -0,0 +1,10 @@
+'use strict';
+// declare the Variable message
+let message = 'hello';
+// Print message
+console.log(message)
+// Assign new value to Variable message
+let message = 'World'
+
+console.log(message)
+
