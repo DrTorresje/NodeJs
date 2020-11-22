430c6d266160011c51af454286a59ee40432f620
Jorge Torres <dr.torres@playahealth.org>
2020-11-22 14:14:13 +0100

    Introduction to comparison Operators

---
+++ compare_numbers.js
@@ -0,0 +1,5 @@
+'use strict';
+let daysInWeek = 7
+console.log('Days in Week equal to 7' ,daysInWeek==7);
+console.log('Days in Week not equal to 7' ,daysInWeek!==7);
+// Identity Test === console.log('Days in Week equal to 7' ,daysInWeek===7);
