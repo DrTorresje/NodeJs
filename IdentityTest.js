d12dc9fe9fce6a7d372201f50f3943ba94290184
Jorge Torres <dr.torres@playahealth.org>
2020-11-26 05:06:46 +0100

    learning about Identity tests in Java

---
+++ IdentityTest.js
@@ -0,0 +1,8 @@
+'use strict';
+let days = 7
+console.log(days);
+console.log(days=='7');
+console.log(days==='7');
+// not equal to has only two 2 signs and not 3
+console.log(days!='7');
+console.log(days!=='7');
---
+++ compareStrings.js
@@ -0,0 +1,5 @@
+'use strict';
+let text = 'cat'
+console.log (text == 'cat')
+console.log(text < 'd ')
+console.log (text !== 'cat')
